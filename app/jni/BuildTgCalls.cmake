55bb13df656a7cd9f2b6508c1fbdc79c60d8b0ef
fomchikmax <fomin3fm@gmail.com>
2026-08-23 13:23:07 +0300

    Remove CustomDcSctpSocket.cpp to match pinned tgcalls commit

--- app/jni/BuildTgCalls.cmake
+++ app/jni/BuildTgCalls.cmake
@@ -67,7 +67,6 @@ add_library(tgcalls STATIC
   "${TGCALLS_DIR}/tgcalls/v2/SignalingEncryption.cpp"
   "${TGCALLS_DIR}/tgcalls/v2/SignalingConnection.cpp"
   "${TGCALLS_DIR}/tgcalls/v2/SignalingSctpConnection.cpp"
-  "${TGCALLS_DIR}/tgcalls/v2/CustomDcSctpSocket.cpp"
 )
 
 target_include_directories(tgcalls PRIVATE
